00a05911fe9bc3c619a7f5674fb6fe1f090e65f2
croketillo <jose_carrizosa@hotmail.com>
2025-02-18 17:50:04 +0100

    First commit

---
+++ example.py
@@ -0,0 +1,11 @@
+from timespy import profile
+import time
+# Usage example: exec_time is now accessible as a function attribute
+@profile
+def slow_function():
+    time.sleep(2)
+
+slow_function()  # Execute function
+
+# ✅ Execution time can also be accessed after calling the function
+print(f">>> {slow_function.__name__} Execution time : {slow_function.exec_time:.6f}s")
